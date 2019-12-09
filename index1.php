679c69721d64d2181ae71f9ced3c538008c10677
Rodrigo <Rodrigodofutebol12@gmail.com>
2019-12-09 14:52:24 -0200

    Foram feitas algumas alterações e adicionada outras coisas

---
+++ index1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="pt-br" dir="ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            
+           
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Login</title>
+            <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+            <link rel="stylesheet" href="css/animate.css">
+            <link rel="stylesheet" href="style.css">
+            <link rel="icon" href="icone_salao.png">
+            
+
+        </head>
+        <body>
+            <div class="login-box">
+                <center>
+                        <img src="icone11.png" width="150px" height="150px"class="img2"><Br>
+                            <p>Salão de Beleza</p>
+                        <br>
+                    <div class="textbox">
+
+                        <form method="POST" action="login.php">
+                            <img src="user-3.png" width="25px" height="25px" class="img1">
+                            <input type="text" name="nome" class="textbox1"><br><br>
+                            <img src="padlock.png" width="25px" height="25px" class="img1">
+                            <input type="password" name="senha" class="textbox1"><br><br>
+                            <a href="index.html"><input type="submit" class="btn"  value="Entrar"></a>
+
+                        </form>
+                    </div>   <br>
+                    <a href="index.html" id="link">Voltar</a>
+                    <a href="cadastro.php" id="link">Cadastrar-se</a>
+                </center>
+
+            </div>
+        
+    
+</body>
+</html>
